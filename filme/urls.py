284594061d7d8a2c3d83e468b2f07dc3c56661e2
lucascomaru <lucascomaru20@gmail.com>
2022-09-29 17:03:10 -0300

    Links dinâmicos criados

--- filme/urls.py
+++ filme/urls.py
@@ -3,9 +3,12 @@
 from django.urls import path, include
 from .views import Homepage, Homefilmes, Detalhesfilme
 
+app_name = 'filme' # app name igual ao nome do app
+
+
 urlpatterns = [
-    path('', Homepage.as_view()),
-    path('filmes/', Homefilmes.as_view()),
-    path('filmes/<int:pk>', Detalhesfilme.as_view()) #Primary Key
+    path('', Homepage.as_view(), name='homepage'),
+    path('filmes/', Homefilmes.as_view(), name='homefilmes'),
+    path('filmes/<int:pk>', Detalhesfilme.as_view(), name='detalhesfilme') #Primary Key
 ]
 
