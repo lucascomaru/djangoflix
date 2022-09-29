aaa58715ac75ad1acb1cc0f3b191a4ae10896b78
lucascomaru <lucascomaru20@gmail.com>
2022-09-29 17:48:46 -0300

    Modelo de episódios criado

--- filme/models.py
+++ filme/models.py
@@ -22,5 +22,21 @@ class Filme(models.Model):
 
 # Criar episódios
 
+class Episodio(models.Model):
+    filme = models.ForeignKey("Filme", related_name="episodios", on_delete=models.CASCADE)
+    titulo = models.CharField(max_length=100)
+    video = models.URLField()
+
+    def __str__(self):
+        return self.filme.titulo + " - "+ self.titulo
+    # Para não aparecer no admin episodio.object
+
+ # Foreignkey =Cria um item dentro do episódio, cria um item dentro de filmes, um episódio só pode
+    # ter um único filme relacionado a ele
+    #Primeiro parametro: Nome da tabela com que ele tem que se relacionar, entre aspas
+    #Segundo parametro: related name
+    #Terceiro parametro: Models.CASCADE - Caso o filme seja deletado, ele vai deletar todos os episodios relacionados
+# a aquele filme
+
 
 # Criar usuário
