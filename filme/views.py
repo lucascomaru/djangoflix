fc4adf0c1ccf7fb3141544549fa1973976f9ab86
lucascomaru <lucascomaru20@gmail.com>
2022-10-03 17:44:11 -0300

    Página de detalhes do filme finalizada

--- filme/views.py
+++ filme/views.py
@@ -3,18 +3,8 @@ from .models import Filme
 from django.views.generic import TemplateView, ListView, DetailView
 # Create your views here.
 
-#def homepage(requests):
-#    return render(requests, "homepage.html")
-
 class Homepage(TemplateView):
     template_name = "homepage.html"
-
-#def homefilmes(request):
-#   context = {}
-#    lista_filmes = Filme.objects()
-#    context['lista_filmes'] = lista_filmes
-#    return render(request, "homefilmes.html", context)
-
 class Homefilmes(ListView):
     template_name = "homefilmes.html"
     model = Filme
@@ -25,9 +15,26 @@ class Detalhesfilme(DetailView):
     model = Filme
     #object -> 1 item do modelo
 
+    def get(self, request, *args, **kwargs):
+        filme = self.get_object()  #descobrir qual filme ele está acessando
+        filme.visualizacoes += 1  #somar 1 nas visualizações daquele filme
+        filme.save()  #salvar
+        return super().get(request, args, kwargs) # redireciona o usuário para o link final
+
     def get_context_data(self, **kwargs):
         context = super(Detalhesfilme, self).get_context_data(**kwargs)
         # Filtrar a tabela de filmes pegando os filmes de categoria igual (object)
         filmes_relacionados = Filme.objects.filter(categoria=self.get_object().categoria)
         context["filmes_relacionados"] = filmes_relacionados
         return context
+
+
+#def homefilmes(request):
+#   context = {}
+#    lista_filmes = Filme.objects()
+#    context['lista_filmes'] = lista_filmes
+#    return render(request, "homefilmes.html", context)
+
+
+#def homepage(requests):
+#    return render(requests, "homepage.html")
