f40feb25d4cb683f8e04a8d8b182bf10e3e1cb15
lucascomaru <lucascomaru20@gmail.com>
2022-10-09 10:51:31 -0300

    Finalizado detalhes de editar perfil

--- filme/views.py
+++ filme/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, reverse
 from .models import Filme, Usuario
 from .forms import CriarContaForm, FormHomepage
-from django.views.generic import TemplateView, ListView, DetailView, FormView
+from django.views.generic import TemplateView, ListView, DetailView, FormView, UpdateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 # Create your views here.
 
@@ -60,8 +60,13 @@ class Pesquisafilme(LoginRequiredMixin, ListView):
         else:
             return None
 
-class Paginaperfil(LoginRequiredMixin, TemplateView):
+class Paginaperfil(LoginRequiredMixin, UpdateView):
     template_name = 'editarperfil.html'
+    model = Usuario
+    fields = ['first_name', 'last_name', 'email']
+
+    def get_success_url(self):
+        return reverse('filme:homefilmes')
 
 class Criarconta(FormView):
     template_name = 'criarconta.html'
